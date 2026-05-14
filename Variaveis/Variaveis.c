af1759320112adc6f1fdd6d08e742f1f03e0ea65
ivalenojose-cloud <ivalenojose@gmail.com>
2026-05-14 22:24:51 +0000

    Implantei comentários sobre os tipos de varáveis

--- Variaveis/Variaveis.c
+++ Variaveis/Variaveis.c
@@ -3,11 +3,14 @@
 int main() {
 
     //Declarar tipo e nome da váriavel
-int idade;
-int quantidade;
-float altura;
-char letra;
-char nome[];
+int idade = 25; // Declara uma variável inteira chamada "idade"
+int quantidade = 1;
+float altura = 1.75; // Declara uma variável de ponto flutuante chamada "altura"
+double peso = 90.3; // Declara uma variável de ponto flutuante de dupla precisão chamada "peso"
+char letra = 'A'; // Declara uma variável do tipo caractere chamada "letra"
+char nome[20] = "Sergio";
+
+
 
 
 
