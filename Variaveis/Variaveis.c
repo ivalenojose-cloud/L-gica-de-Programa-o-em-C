86efa9ba4dcffc8542443c08934919a8e1f19ac7
ivalenojose-cloud <ivalenojose@gmail.com>
2026-05-15 23:06:35 +0000

    Implantei especificadores de formato

--- Variaveis/Variaveis.c
+++ Variaveis/Variaveis.c
@@ -2,16 +2,25 @@
 
 int main() {
 
-    //Declarar tipo e nome da váriavel
-int idade = 25; // Declara uma variável inteira chamada "idade"
-int quantidade = 1;
+int idade = 25;// Declara uma variável inteira chamada "idade"
 float altura = 1.75; // Declara uma variável de ponto flutuante chamada "altura"
-double peso = 90.3; // Declara uma variável de ponto flutuante de dupla precisão chamada "peso"
-char letra = 'A'; // Declara uma variável do tipo caractere chamada "letra"
-char nome[20] = "Sergio";
+char opcao = 'S'; // Declara uma variável do tipo caractere chamada "letra"
+char nome[20] = "Sergio"; // Declara um array de 20 caracteres e inicializa com "Sergio"
 
 
+printf("A idade do %s é: %d\n", nome, idade);
+printf("A altura é: %f", altura);
+printf("A opção é: %c\n", opcao);
 
+/*
+printf("%formato1, %formato2, %formato3, varialvel1, variavel2, variavel3")
+%d: Imprime um inteiro em formato decimal.
+%i: Equivalente a %d.
+%f: Imprime um número de ponto flutuante no formato padrão.
+%e: Imprime um número de ponto flutuante na notação científica.
+%c: Imprime um único caractere.
+%s: Imprime uma cadeia (string) de caracteres.
+*/
 
 
 
