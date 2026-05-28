b5b710babb3a9f997b1a2a631ad94db7cb881174
ivalenojose-cloud <ivalenojose@gmail.com>
2026-05-28 00:42:54 +0000

    na lógica da divisão inseri o (float) para exibir no terminal o resultado corretamente em números decimais.

--- Variaveis/Função Scanf/Função Fgets/Cadastro de Alunos/Operadores_matematicos/Operadores_de_atribuicao/Incremento_Decremento/Testes/Testes.c
+++ Variaveis/Função Scanf/Função Fgets/Cadastro de Alunos/Operadores_matematicos/Operadores_de_atribuicao/Incremento_Decremento/Testes/Testes.c
@@ -2,20 +2,26 @@
 
 int main() {
 
-float numero1, numero2;
+int numero1, numero2;
+int soma, subtracao, multiplicacao;
 float divisao;
 
 printf("Digite o numero 1: ");
-scanf("%f", &numero1);
+scanf("%d", &numero1);
 
 printf("Digite o numero 2: ");
-scanf("%f", &numero2);
+scanf("%d", &numero2);
 
-divisao = numero1 / numero2;
+soma = numero1 + numero2;
+subtracao = numero1 - numero2;
+multiplicacao = numero1 * numero2;
+divisao = numero1 / (float) numero2;
 
-printf("O resultado da divisão é: %f\n", divisao);
+printf("A soma é: %d\n", soma);
+printf("A subtração é: %d\n", subtracao);
+printf("A multiplicação é: %d\n", multiplicacao);
+printf("A divisao é: %.2f\n", divisao);
 
 return 0;
-
 
 }
--- Variaveis/Função Scanf/Função Fgets/Cadastro de Alunos/Operadores_matematicos/operadores_matematicos.c
+++ Variaveis/Função Scanf/Função Fgets/Cadastro de Alunos/Operadores_matematicos/operadores_matematicos.c
@@ -10,11 +10,12 @@ int main() {
     */
 
     int numero1, numero2;
-    int soma, subtracao, multiplicacao, divisao;
+    int soma, subtracao, multiplicacao;
+    float divisao;
 
-    printf("Entre com o número 1: \n");
+    printf("Entre com o número 1: ");
     scanf("%d", &numero1);
-    printf("Entre com o número 2: \n");
+    printf("Entre com o número 2: ");
     scanf("%d", &numero2);
     
     //Operação de Soma
@@ -27,12 +28,12 @@ int main() {
     multiplicacao = numero1 * numero2;
  
     //Operação de Divisão
-    divisao = numero1 / numero2;
+    divisao = numero1 /(float) numero2; // Inserir (float) para no terminal exibir os numeros decimais corretamente.
 
     printf("A soma é: %d\n", soma);
     printf("A subtração é: %d\n", subtracao);
     printf("A multiplicação é: %d\n", multiplicacao);
-    printf("A divisão é: %d\n", divisao);
+    printf("A divisão é: %.2f\n", divisao);
 
     return 0;
      
